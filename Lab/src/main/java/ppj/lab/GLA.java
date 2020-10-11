55fe892cde4aca84c6af6c3f9fd863296531d6c1
Matej Čubek <33391902+MCubek@users.noreply.github.com>
2020-10-11 23:40:11 +0200

    Added parsing of regex,states and uniform simbols in generator constructor.

--- Lab/src/main/java/ppj/lab/GLA.java
+++ Lab/src/main/java/ppj/lab/GLA.java
@@ -1,4 +1,37 @@
 package ppj.lab;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class GLA {
+    private Map<String, String> regex;
+    private Set<String> states;
+    private Set<String> uniformSimbols;
+
+    public GLA(File inputFile) throws FileNotFoundException {
+        if (inputFile == null) throw new IllegalArgumentException("File does not exist");
+
+        Scanner scanner = new Scanner(inputFile);
+        regex = new HashMap<>();
+
+        String line;
+        //Populirati regex
+        while ((line = scanner.nextLine()).matches("\\{..*} .*")) {
+            //Pronadi index kraja imena
+            int index = line.indexOf('}');
+            regex.put(line.substring(1, index - 1), line.substring(index + 1));
+        }
+
+        //Populiraj stanja
+        if (! scanner.hasNextLine() && ! (line = scanner.nextLine()).matches("%X .*"))
+            throw new IllegalArgumentException("File missing states description");
+        states = Arrays.stream(line.substring(2).split(" ")).collect(Collectors.toSet());
+
+        //Populiraj leksicke jedinke
+        if (! scanner.hasNextLine() && ! (line = scanner.nextLine()).matches("%L .*"))
+            throw new IllegalArgumentException("File missing states description");
+        uniformSimbols = Arrays.stream(line.substring(2).split(" ")).collect(Collectors.toSet());
+    }
 }
