df1136ca8321582d3b5fab51d3c2c3b29c29d1bb
Frane Batagelj <frane.batagelj@fer.hr>
2020-10-16 21:17:11 +0200

    Dodane klase Pair, Automaton i AutomatonGenerator te prikladne funkcije

---
+++ Lab/src/main/java/ppj/lab/Pair.java
@@ -0,0 +1,41 @@
+package ppj.lab;
+
+import java.util.Objects;
+
+/**
+ * Klasa koja predstavlja par dva atributa proizvoljnih klasa
+ *
+ * @author MatejC FraneB
+ */
+public class Pair<T,V> {
+
+    T left;
+    V right;
+
+    public Pair(T left, V right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public T getLeft() {
+        return left;
+    }
+
+    public V getRight() {
+        return right;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(left, pair.left) &&
+                Objects.equals(right, pair.right);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(left, right);
+    }
+}
