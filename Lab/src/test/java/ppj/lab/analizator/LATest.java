da84967ab21b0b7f763b7f2d09b9aafc441bfb19
Matej Čubek <33391902+MCubek@users.noreply.github.com>
2020-10-26 14:43:28 +0100

    moved test resources folder

--- Lab/src/test/java/ppj/lab/analizator/LATest.java
+++ Lab/src/test/java/ppj/lab/analizator/LATest.java
@@ -16,12 +16,12 @@ public class LATest {
 
     @Test
     public void testLAExample0() throws IOException, ClassNotFoundException {
-        File file = new File("src/main/resources/lab1_ppjLang[1].txt");
+        File file = new File("src/test/resources/lab1_ppjLang[1].txt");
         //System.out.println(file.getAbsolutePath());
         GLA gla = new GLA(file);
         gla.serializeOutput();
 
-        file = new File("src/main/resources/lab1_program1[1].c");
+        file = new File("src/test/resources/lab1_program1[1].c");
         //System.out.println(file.getAbsolutePath());
         LA la = new LA(file);
 
@@ -32,254 +32,254 @@ public class LATest {
 
     @Test
     public void testOrginalProblem1() throws IOException, ClassNotFoundException {
-        File file = new File("src/main/resources/orignalPrimjeri/minusLang.lan");
+        File file = new File("src/test/resources/orignalPrimjeri/minusLang.lan");
         GLA gla = new GLA(file);
         gla.serializeOutput();
 
-        file = new File("src/main/resources/orignalPrimjeri/minusLang.in");
+        file = new File("src/test/resources/orignalPrimjeri/minusLang.in");
         LA la = new LA(file);
 
-        assertEquals(readFileAsStringFromResources("src/main/resources/orignalPrimjeri/minusLang.out"), la.getOutputAsString());
+        assertEquals(readFileAsStringFromResources("src/test/resources/orignalPrimjeri/minusLang.out"), la.getOutputAsString());
     }
 
     @Test
     public void testOrginalProblem2() throws IOException, ClassNotFoundException {
-        File file = new File("src/main/resources/orignalPrimjeri/nadji_a1.lan");
+        File file = new File("src/test/resources/orignalPrimjeri/nadji_a1.lan");
         GLA gla = new GLA(file);
         gla.serializeOutput();
 
-        file = new File("src/main/resources/orignalPrimjeri/nadji_a1.in");
+        file = new File("src/test/resources/orignalPrimjeri/nadji_a1.in");
         LA la = new LA(file);
 
-        assertEquals(readFileAsStringFromResources("src/main/resources/orignalPrimjeri/nadji_a1.out"), la.getOutputAsString());
+        assertEquals(readFileAsStringFromResources("src/test/resources/orignalPrimjeri/nadji_a1.out"), la.getOutputAsString());
     }
 
     @Test
     public void testOrginalProblem3() throws IOException, ClassNotFoundException {
-        File file = new File("src/main/resources/orignalPrimjeri/nadji_a2.lan");
+        File file = new File("src/test/resources/orignalPrimjeri/nadji_a2.lan");
         GLA gla = new GLA(file);
         gla.serializeOutput();
 
-        file = new File("src/main/resources/orignalPrimjeri/nadji_a2.in");
+        file = new File("src/test/resources/orignalPrimjeri/nadji_a2.in");
         LA la = new LA(file);
 
-        assertEquals(readFileAsStringFromResources("src/main/resources/orignalPrimjeri/nadji_a2.out"), la.getOutputAsString());
+        assertEquals(readFileAsStringFromResources("src/test/resources/orignalPrimjeri/nadji_a2.out"), la.getOutputAsString());
     }
 
     @Test
     public void testOrginalProblem4() throws IOException, ClassNotFoundException {
-        File file = new File("src/main/resources/orignalPrimjeri/simplePpjLang.lan");
+        File file = new File("src/test/resources/orignalPrimjeri/simplePpjLang.lan");
         GLA gla = new GLA(file);
         gla.serializeOutput();
 
-        file = new File("src/main/resources/orignalPrimjeri/simplePpjLang.in");
+        file = new File("src/test/resources/orignalPrimjeri/simplePpjLang.in");
         LA la = new LA(file);
 
-        assertEquals(readFileAsStringFromResources("src/main/resources/orignalPrimjeri/simplePpjLang.out"), la.getOutputAsString());
+        assertEquals(readFileAsStringFromResources("src/test/resources/orignalPrimjeri/simplePpjLang.out"), la.getOutputAsString());
     }
 
     @Test
     public void testOrginalProblem5() throws IOException, ClassNotFoundException {
-        File file = new File("src/main/resources/orignalPrimjeri/svaki_drugi_a1.lan");
+        File file = new File("src/test/resources/orignalPrimjeri/svaki_drugi_a1.lan");
         GLA gla = new GLA(file);
         gla.serializeOutput();
 
-        file = new File("src/main/resources/orignalPrimjeri/svaki_drugi_a1.in");
+        file = new File("src/test/resources/orignalPrimjeri/svaki_drugi_a1.in");
         LA la = new LA(file);
 
-        assertEquals(readFileAsStringFromResources("src/main/resources/orignalPrimjeri/svaki_drugi_a1.out"), la.getOutputAsString());
+        assertEquals(readFileAsStringFromResources("src/test/resources/orignalPrimjeri/svaki_drugi_a1.out"), la.getOutputAsString());
     }
 
     @Test
     public void testOrginalProblem6() throws IOException, ClassNotFoundException {
-        File file = new File("src/main/resources/orignalPrimjeri/svaki_drugi_a2.lan");
+        File file = new File("src/test/resources/orignalPrimjeri/svaki_drugi_a2.lan");
         GLA gla = new GLA(file);
         gla.serializeOutput();
 
-        file = new File("src/main/resources/orignalPrimjeri/svaki_drugi_a2.in");
+        file = new File("src/test/resources/orignalPrimjeri/svaki_drugi_a2.in");
         LA la = new LA(file);
 
-        assertEquals(readFileAsStringFromResources("src/main/resources/orignalPrimjeri/svaki_drugi_a2.out"), la.getOutputAsString());
+        assertEquals(readFileAsStringFromResources("src/test/resources/orignalPrimjeri/svaki_drugi_a2.out"), la.getOutputAsString());
     }
 
     @Test
     public void testExamProblem1() throws IOException, ClassNotFoundException {
-        File file = new File("src/main/resources/ispitniTestovi/01_nadji_x/test.lan");
+        File file = new File("src/test/resources/ispitniTestovi/01_nadji_x/test.lan");
         GLA gla = new GLA(file);
         gla.serializeOutput();
 
-        file = new File("src/main/resources/ispitniTestovi/01_nadji_x/test.in");
+        file = new File("src/test/resources/ispitniTestovi/01_nadji_x/test.in");
         LA la = new LA(file);
 
-        assertEquals(readFileAsStringFromResources("src/main/resources/ispitniTestovi/01_nadji_x/test.out"), la.getOutputAsString());
+        assertEquals(readFileAsStringFromResources("src/test/resources/ispitniTestovi/01_nadji_x/test.out"), la.getOutputAsString());
     }
 
     @Test
     public void testExamProblem3() throws IOException, ClassNotFoundException {
-        File file = new File("src/main/resources/ispitniTestovi/03_nadji_x_oporavak/test.lan");
+        File file = new File("src/test/resources/ispitniTestovi/03_nadji_x_oporavak/test.lan");
         GLA gla = new GLA(file);
         gla.serializeOutput();
 
-        file = new File("src/main/resources/ispitniTestovi/03_nadji_x_oporavak/test.in");
+        file = new File("src/test/resources/ispitniTestovi/03_nadji_x_oporavak/test.in");
         LA la = new LA(file);
 
-        assertEquals(readFileAsStringFromResources("src/main/resources/ispitniTestovi/03_nadji_x_oporavak/test.out"), la.getOutputAsString());
+        assertEquals(readFileAsStringFromResources("src/test/resources/ispitniTestovi/03_nadji_x_oporavak/test.out"), la.getOutputAsString());
     }
 
     @Test
     public void testExamProblem5() throws IOException, ClassNotFoundException {
-        File file = new File("src/main/resources/ispitniTestovi/05_regex_tezi/test.lan");
+        File file = new File("src/test/resources/ispitniTestovi/05_regex_tezi/test.lan");
         GLA gla = new GLA(file);
         gla.serializeOutput();
 
-        file = new File("src/main/resources/ispitniTestovi/05_regex_tezi/test.in");
+        file = new File("src/test/resources/ispitniTestovi/05_regex_tezi/test.in");
         LA la = new LA(file);
 
-        assertEquals(readFileAsStringFromResources("src/main/resources/ispitniTestovi/05_regex_tezi/test.out"), la.getOutputAsString());
+        assertEquals(readFileAsStringFromResources("src/test/resources/ispitniTestovi/05_regex_tezi/test.out"), la.getOutputAsString());
     }
 
     @Test
     public void testExamProblem7() throws IOException, ClassNotFoundException {
-        File file = new File("src/main/resources/ispitniTestovi/07_regex_regdefs/test.lan");
+        File file = new File("src/test/resources/ispitniTestovi/07_regex_regdefs/test.lan");
         GLA gla = new GLA(file);
         gla.serializeOutput();
 
-        file = new File("src/main/resources/ispitniTestovi/07_regex_regdefs/test.in");
+        file = new File("src/test/resources/ispitniTestovi/07_regex_regdefs/test.in");
         LA la = new LA(file);
 
-        assertEquals(readFileAsStringFromResources("src/main/resources/ispitniTestovi/07_regex_regdefs/test.out"), la.getOutputAsString());
+        assertEquals(readFileAsStringFromResources("src/test/resources/ispitniTestovi/07_regex_regdefs/test.out"), la.getOutputAsString());
     }
 
     @Test
     public void testExamProblem9() throws IOException, ClassNotFoundException {
-        File file = new File("src/main/resources/ispitniTestovi/09_poredak/test.lan");
+        File file = new File("src/test/resources/ispitniTestovi/09_poredak/test.lan");
         GLA gla = new GLA(file);
         gla.serializeOutput();
 
-        file = new File("src/main/resources/ispitniTestovi/09_poredak/test.in");
+        file = new File("src/test/resources/ispitniTestovi/09_poredak/test.in");
         LA la = new LA(file);
 
-        assertEquals(readFileAsStringFromResources("src/main/resources/ispitniTestovi/09_poredak/test.out"), la.getOutputAsString());
+        assertEquals(readFileAsStringFromResources("src/test/resources/ispitniTestovi/09_poredak/test.out"), la.getOutputAsString());
     }
 
     @Test
     public void testExamProblem11() throws IOException, ClassNotFoundException {
-        File file = new File("src/main/resources/ispitniTestovi/11_state_hopper/test.lan");
+        File file = new File("src/test/resources/ispitniTestovi/11_state_hopper/test.lan");
         GLA gla = new GLA(file);
         gla.serializeOutput();
 
-        file = new File("src/main/resources/ispitniTestovi/11_state_hopper/test.in");
+        file = new File("src/test/resources/ispitniTestovi/11_state_hopper/test.in");
         LA la = new LA(file);
 
-        assertEquals(readFileAsStringFromResources("src/main/resources/ispitniTestovi/11_state_hopper/test.out"), la.getOutputAsString());
+        assertEquals(readFileAsStringFromResources("src/test/resources/ispitniTestovi/11_state_hopper/test.out"), la.getOutputAsString());
     }
 
     @Test
     public void testExamProblem13() throws IOException, ClassNotFoundException {
-        File file = new File("src/main/resources/ispitniTestovi/13_vrati_se/test.lan");
+        File file = new File("src/test/resources/ispitniTestovi/13_vrati_se/test.lan");
         GLA gla = new GLA(file);
         gla.serializeOutput();
 
-        file = new File("src/main/resources/ispitniTestovi/13_vrati_se/test.in");
+        file = new File("src/test/resources/ispitniTestovi/13_vrati_se/test.in");
         LA la = new LA(file);
 
-        assertEquals(readFileAsStringFromResources("src/main/resources/ispitniTestovi/13_vrati_se/test.out"), la.getOutputAsString());
+        assertEquals(readFileAsStringFromResources("src/test/resources/ispitniTestovi/13_vrati_se/test.out"), la.getOutputAsString());
     }
 
     @Test
     public void testExamProblem15() throws IOException, ClassNotFoundException {
-        File file = new File("src/main/resources/ispitniTestovi/15_minusLang_laksi/test.lan");
+        File file = new File("src/test/resources/ispitniTestovi/15_minusLang_laksi/test.lan");
         GLA gla = new GLA(file);
         gla.serializeOutput();
 
-        file = new File("src/main/resources/ispitniTestovi/15_minusLang_laksi/test.in");
+        file = new File("src/test/resources/ispitniTestovi/15_minusLang_laksi/test.in");
         LA la = new LA(file);
 
-        assertEquals(readFileAsStringFromResources("src/main/resources/ispitniTestovi/15_minusLang_laksi/test.out"), la.getOutputAsString());
+        assertEquals(readFileAsStringFromResources("src/test/resources/ispitniTestovi/15_minusLang_laksi/test.out"), la.getOutputAsString());
     }
 
     @Test
     public void testExamProblem17() throws IOException, ClassNotFoundException {
-        File file = new File("src/main/resources/ispitniTestovi/17_simplePpjLang_laksi/test.lan");
+        File file = new File("src/test/resources/ispitniTestovi/17_simplePpjLang_laksi/test.lan");
         GLA gla = new GLA(file);
         gla.serializeOutput();
 
-        file = new File("src/main/resources/ispitniTestovi/17_simplePpjLang_laksi/test.in");
+        file = new File("src/test/resources/ispitniTestovi/17_simplePpjLang_laksi/test.in");
         LA la = new LA(file);
 
-        assertEquals(readFileAsStringFromResources("src/main/resources/ispitniTestovi/17_simplePpjLang_laksi/test.out"), la.getOutputAsString());
+        assertEquals(readFileAsStringFromResources("src/test/resources/ispitniTestovi/17_simplePpjLang_laksi/test.out"), la.getOutputAsString());
     }
 
     @Test
     public void testExamProblem19() throws IOException, ClassNotFoundException {
-        File file = new File("src/main/resources/ispitniTestovi/19_ppjLang_laksi/test.lan");
+        File file = new File("src/test/resources/ispitniTestovi/19_ppjLang_laksi/test.lan");
         GLA gla = new GLA(file);
         gla.serializeOutput();
 
-        file = new File("src/main/resources/ispitniTestovi/19_ppjLang_laksi/test.in");
+        file = new File("src/test/resources/ispitniTestovi/19_ppjLang_laksi/test.in");
         LA la = new LA(file);
 
-        assertEquals(readFileAsStringFromResources("src/main/resources/ispitniTestovi/19_ppjLang_laksi/test.out"), la.getOutputAsString());
+        assertEquals(readFileAsStringFromResources("src/test/resources/ispitniTestovi/19_ppjLang_laksi/test.out"), la.getOutputAsString());
     }
 
     @Test
     public void testExamProblem21() throws IOException, ClassNotFoundException {
-        File file = new File("src/main/resources/ispitniTestovi/21_kom4/test.lan");
+        File file = new File("src/test/resources/ispitniTestovi/21_kom4/test.lan");
         GLA gla = new GLA(file);
         gla.serializeOutput();
 
-        file = new File("src/main/resources/ispitniTestovi/21_kom4/test.in");
+        file = new File("src/test/resources/ispitniTestovi/21_kom4/test.in");
         LA la = new LA(file);
 
-        assertEquals(readFileAsStringFromResources("src/main/resources/ispitniTestovi/21_kom4/test.out"), la.getOutputAsString());
+        assertEquals(readFileAsStringFromResources("src/test/resources/ispitniTestovi/21_kom4/test.out"), la.getOutputAsString());
     }
 
     @Test
     public void testExamProblem23() throws IOException, ClassNotFoundException {
-        File file = new File("src/main/resources/ispitniTestovi/23_op4/test.lan");
+        File file = new File("src/test/resources/ispitniTestovi/23_op4/test.lan");
         GLA gla = new GLA(file);
         gla.serializeOutput();
 
-        file = new File("src/main/resources/ispitniTestovi/23_op4/test.in");
+        file = new File("src/test/resources/ispitniTestovi/23_op4/test.in");
         LA la = new LA(file);
 
-        assertEquals(readFileAsStringFromResources("src/main/resources/ispitniTestovi/23_op4/test.out"), la.getOutputAsString());
+        assertEquals(readFileAsStringFromResources("src/test/resources/ispitniTestovi/23_op4/test.out"), la.getOutputAsString());
     }
 
     @Test
     public void testExamProblem25() throws IOException, ClassNotFoundException {
-        File file = new File("src/main/resources/ispitniTestovi/25_prog3/test.lan");
+        File file = new File("src/test/resources/ispitniTestovi/25_prog3/test.lan");
         GLA gla = new GLA(file);
         gla.serializeOutput();
 
-        file = new File("src/main/resources/ispitniTestovi/25_prog3/test.in");
+        file = new File("src/test/resources/ispitniTestovi/25_prog3/test.in");
         LA la = new LA(file);
 
-        assertEquals(readFileAsStringFromResources("src/main/resources/ispitniTestovi/25_prog3/test.out"), la.getOutputAsString());
+        assertEquals(readFileAsStringFromResources("src/test/resources/ispitniTestovi/25_prog3/test.out"), la.getOutputAsString());
     }
 
     @Test
     public void testExamProblem27() throws IOException, ClassNotFoundException {
-        File file = new File("src/main/resources/ispitniTestovi/27_prog5/test.lan");
+        File file = new File("src/test/resources/ispitniTestovi/27_prog5/test.lan");
         GLA gla = new GLA(file);
         gla.serializeOutput();
 
-        file = new File("src/main/resources/ispitniTestovi/27_prog5/test.in");
+        file = new File("src/test/resources/ispitniTestovi/27_prog5/test.in");
         LA la = new LA(file);
 
-        assertEquals(readFileAsStringFromResources("src/main/resources/ispitniTestovi/27_prog5/test.out"), la.getOutputAsString());
+        assertEquals(readFileAsStringFromResources("src/test/resources/ispitniTestovi/27_prog5/test.out"), la.getOutputAsString());
     }
 
     @Test
     public void testExamProblem29() throws IOException, ClassNotFoundException {
-        File file = new File("src/main/resources/ispitniTestovi/29_veliki3/test.lan");
+        File file = new File("src/test/resources/ispitniTestovi/29_veliki3/test.lan");
         GLA gla = new GLA(file);
         gla.serializeOutput();
 
-        file = new File("src/main/resources/ispitniTestovi/29_veliki3/test.in");
+        file = new File("src/test/resources/ispitniTestovi/29_veliki3/test.in");
         LA la = new LA(file);
 
-        assertEquals(readFileAsStringFromResources("src/main/resources/ispitniTestovi/29_veliki3/test.out"), la.getOutputAsString());
+        assertEquals(readFileAsStringFromResources("src/test/resources/ispitniTestovi/29_veliki3/test.out"), la.getOutputAsString());
     }
 
     private String readFileAsStringFromResources(String pathName) throws IOException {
