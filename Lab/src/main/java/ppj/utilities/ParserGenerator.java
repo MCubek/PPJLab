2326585f6d763c418ccad9b79bbb28bd05364b7b
Frane Batagelj <fb51694@fer.hr>
2020-11-30 19:26:54 +0100

    Completed ENKA to DKA transformation

--- Lab/src/main/java/ppj/utilities/ParserGenerator.java
+++ Lab/src/main/java/ppj/utilities/ParserGenerator.java
@@ -15,7 +15,7 @@ public class ParserGenerator {
         this.LRProductions = generateAllLRProductions(Objects.requireNonNull(initialProductions), nonTerminalSymbols.get(0));
         firstRelation = new LinkedHashMap<>();
         generateFirstTable(entrySymbols, initialProductions,nonTerminalSymbols,terminalSymbols);
-        generateParser(nonTerminalSymbols,terminalSymbols);
+        generateParserTables(nonTerminalSymbols,terminalSymbols);
     }
 
     public Map<String, List<List<String>>> getLRProductions() {
@@ -191,89 +191,158 @@ public class ParserGenerator {
         }
         }
 
-        public void generateParser(List<String> nonTerminalSymbols, List<String> terminalSymbols) {
-            //mapa koja veze stanja uz produkcije
-            Map<Integer, Pair<Pair<String,List<String>>,String>> productionsToStates = new LinkedHashMap<>();
-            //brojac stanja
-            int stateCounter = 0;
-            //mapa svih prijelaza
-            Map<Pair<Integer,String>,List<Integer>> transitions = new LinkedHashMap<>();
-            //queue nezavrsnih znakova koji se trebaju obraditi
-            Queue<Pair<Pair<String,String>,Integer>> transitionQueue = new LinkedList<>();
-            //u queue se ubacuje pocetna produkcija i izlazni znak {$}
-            Pair<String,String> initialState = new Pair<>(LRProductions.keySet().toArray()[0].toString(),"{$}");
-            Queue<Pair<String,String>> supportQueue = new LinkedList<>();
-            supportQueue.add(initialState);
-            Pair<Pair<String,String>,Integer> initialQueueElement = new Pair<>(initialState,-1);
-            transitionQueue.add(initialQueueElement);
-            //brojimo koje smo parove stanja i izlaznih znakova prosli da se izbjegne rekurzija u beskonacnost
-            Set<Pair<String,String>> removed = new HashSet<>();
-            //obraduje se dok ima nezavrsnih znakova za obraditi
-            while(!transitionQueue.isEmpty()) {
-                //izvlaci se trenutni par za obradu i oznacuje se kao da je vec proden
-                Pair<Pair<String,String>,Integer> currentQueueElement = transitionQueue.remove();
-                Pair<String,String> currentLeftSide = currentQueueElement.getLeft();
-                supportQueue.remove();
-                removed.add(currentLeftSide);
-                Integer previousState = null;
-                String transitionSymbol = null;
-                //gledaju se produkcije nezavrsnog znaka
-                for(List<String> currentProductions : LRProductions.get(currentLeftSide.getLeft())) {
-                    Pair<String,List<String>> beforeProduction = new Pair<>(currentLeftSide.getLeft(),currentProductions);
-                    Pair<Pair<String,List<String>>,String> afterProduction = new Pair<>(beforeProduction,currentLeftSide.getRight());
-                    productionsToStates.put(stateCounter++,afterProduction);
-                    if(currentProductions.indexOf("*") != 0) {
-                        Pair<Integer,String> transitionPair = new Pair<>(previousState,transitionSymbol);
-                        addTransition(stateCounter, transitions, transitionPair);
-                        if(transitionSymbol != null && nonTerminalSymbols.contains(transitionSymbol)) {
-                            StringBuilder followSymbolString = new StringBuilder("{");
-                            String followSymbol;
-                            if(currentProductions.indexOf("*") != currentProductions.size() - 1) {
-                                List<String> checkEmpty = currentProductions.subList(currentProductions.indexOf("*") + 1,currentProductions.size());
-                                if(this.emptySymbols.containsAll(checkEmpty)) {
-                                    followSymbolString.append("$, ");
-                                }
-                                followSymbol = currentProductions.get(currentProductions.indexOf("*") + 1);
-                                if(nonTerminalSymbols.contains(followSymbol)) {
-                                    List<String> firstRelations = firstRelation.get(followSymbol);
-                                    for (String s : firstRelations) {
-                                        if (firstRelations.indexOf(s) == firstRelations.size() - 1)
-                                            followSymbolString.append(s).append("}");
-                                        else
-                                            followSymbolString.append(s).append(", ");
-                                    }
-                                } else {
-                                    followSymbolString.append(followSymbol).append("}");
+        public void generateParserTables(List<String> nonTerminalSymbols, List<String> terminalSymbols) {
+        Pair<Map<Integer, Pair<Pair<String,List<String>>,String>>,Map<Pair<Integer,String>,List<Integer>>> statesAndTransitions = generateENKA(nonTerminalSymbols,terminalSymbols);
+        enkaToDka(statesAndTransitions);
+        }
+
+
+    private void enkaToDka(Pair<Map<Integer, Pair<Pair<String, List<String>>, String>>, Map<Pair<Integer, String>, List<Integer>>> statesAndTransitions) {
+       Map<Integer,Set<Integer>> dkaStates = new LinkedHashMap<>();
+       Map<Pair<Integer,String>,Integer> dkaTransitions = new LinkedHashMap<>();
+       List<Set<Integer>> existingGroups = new ArrayList<>();
+
+       Queue<Pair<Integer,Pair<Integer,String>>> statesToTransform = new LinkedList<>();
+       Queue<Integer> removed = new LinkedList<>();
+       Map<Integer, Pair<Pair<String,List<String>>,String>> enkaStates = statesAndTransitions.getLeft();
+       Map<Pair<Integer, String>, List<Integer>> enkaTransitions = statesAndTransitions.getRight();
+       statesToTransform.add(new Pair<>((Integer) enkaStates.keySet().toArray()[0], new Pair<>(-1,"")));
+       int stateCounter = 0;
+       while(!statesToTransform.isEmpty()) {
+           Pair<Integer,Pair<Integer,String>> currentElement = statesToTransform.remove();
+           Integer currentState = currentElement.getLeft();
+           Pair<Integer,String> fromState = currentElement.getRight();
+           removed.add(currentState);
+
+           Set<Integer> groupedStates = calculateEClosure(currentState,enkaTransitions);
+           boolean checkifExists = true;
+           for(Set<Integer> group : existingGroups) {
+               if (groupedStates.equals(group)) {
+                   checkifExists = false;
+                   break;
+               }
+           }
+           if(checkifExists) {
+               dkaStates.put(stateCounter++, groupedStates);
+               existingGroups.add(groupedStates);
+           }
+
+           if(fromState.getLeft() != -1) {
+               dkaTransitions.put(new Pair<>(fromState.getLeft(), fromState.getRight()),stateCounter-1);
+           }
+
+           for(String symbol : this.entrySymbols) {
+               for(Integer state : groupedStates) {
+                   Pair<Integer,String> checkState = new Pair<>(state,symbol);
+                   if(enkaTransitions.containsKey(checkState))
+                       for(Integer nextState : enkaTransitions.get(checkState))
+                           statesToTransform.add(new Pair<>(nextState,new Pair<>(stateCounter-1,symbol)));
+               }
+           }
+       }
+    }
+
+    private Set<Integer> calculateEClosure(Integer currentState, Map<Pair<Integer, String>, List<Integer>> transitions) {
+        Set<Integer> groupedStates = new HashSet<>();
+        Queue<Integer> findEClosure = new LinkedList<>();
+        findEClosure.add(currentState);
+        groupedStates.add(currentState);
+        while(!findEClosure.isEmpty()) {
+            Integer eClosureState = findEClosure.remove();
+            Pair<Integer, String> eTransition = new Pair<>(eClosureState, "$");
+            if(transitions.containsKey(eTransition)) {
+                groupedStates.addAll(transitions.get(eTransition));
+                findEClosure.addAll(transitions.get(eTransition));
+            }
+        }
+        return groupedStates;
+    }
+
+    private Pair<Map<Integer, Pair<Pair<String,List<String>>,String>>,Map<Pair<Integer,String>,List<Integer>>> generateENKA(List<String> nonTerminalSymbols, List<String> terminalSymbols) {
+        //mapa koja veze stanja uz produkcije
+        Map<Integer, Pair<Pair<String,List<String>>,String>> productionsToStates = new LinkedHashMap<>();
+        //brojac stanja
+        int stateCounter = 0;
+        //mapa svih prijelaza
+        Map<Pair<Integer,String>,List<Integer>> transitions = new LinkedHashMap<>();
+        //queue nezavrsnih znakova koji se trebaju obraditi
+        Queue<Pair<Pair<String,String>,Integer>> transitionQueue = new LinkedList<>();
+        //u queue se ubacuje pocetna produkcija i izlazni znak {$}
+        Pair<String,String> initialState = new Pair<>(LRProductions.keySet().toArray()[0].toString(),"{$}");
+        Queue<Pair<String,String>> supportQueue = new LinkedList<>();
+        supportQueue.add(initialState);
+        Pair<Pair<String,String>,Integer> initialQueueElement = new Pair<>(initialState,-1);
+        transitionQueue.add(initialQueueElement);
+        //brojimo koje smo parove stanja i izlaznih znakova prosli da se izbjegne rekurzija u beskonacnost
+        Set<Pair<String,String>> removed = new HashSet<>();
+        //obraduje se dok ima nezavrsnih znakova za obraditi
+        while(!transitionQueue.isEmpty()) {
+            //izvlaci se trenutni par za obradu i oznacuje se kao da je vec proden
+            Pair<Pair<String,String>,Integer> currentQueueElement = transitionQueue.remove();
+            Pair<String,String> currentLeftSide = currentQueueElement.getLeft();
+            supportQueue.remove();
+            removed.add(currentLeftSide);
+            Integer previousState = null;
+            String transitionSymbol = null;
+            //gledaju se produkcije nezavrsnog znaka
+            for(List<String> currentProductions : LRProductions.get(currentLeftSide.getLeft())) {
+                Pair<String,List<String>> beforeProduction = new Pair<>(currentLeftSide.getLeft(),currentProductions);
+                Pair<Pair<String,List<String>>,String> afterProduction = new Pair<>(beforeProduction,currentLeftSide.getRight());
+                productionsToStates.put(stateCounter++,afterProduction);
+                if(currentProductions.indexOf("*") != 0) {
+                    Pair<Integer,String> transitionPair = new Pair<>(previousState,transitionSymbol);
+                    addTransition(stateCounter, transitions, transitionPair);
+                    if(transitionSymbol != null && nonTerminalSymbols.contains(transitionSymbol)) {
+                        StringBuilder followSymbolString = new StringBuilder("{");
+                        String followSymbol;
+                        if(currentProductions.indexOf("*") != currentProductions.size() - 1) {
+                            List<String> checkEmpty = currentProductions.subList(currentProductions.indexOf("*") + 1,currentProductions.size());
+                            if(this.emptySymbols.containsAll(checkEmpty)) {
+                                followSymbolString.append("$, ");
+                            }
+                            followSymbol = currentProductions.get(currentProductions.indexOf("*") + 1);
+                            if(nonTerminalSymbols.contains(followSymbol)) {
+                                List<String> firstRelations = firstRelation.get(followSymbol);
+                                for (String s : firstRelations) {
+                                    if (firstRelations.indexOf(s) == firstRelations.size() - 1)
+                                        followSymbolString.append(s).append("}");
+                                    else
+                                        followSymbolString.append(s).append(", ");
                                 }
                             } else {
-                                followSymbolString.append("$}");
+                                followSymbolString.append(followSymbol).append("}");
                             }
-                            Pair<String,String> nextPair = new Pair<>(transitionSymbol, followSymbolString.toString());
-                            Pair<Pair<String,String>,Integer> nextQueueElement = new Pair<>(nextPair,previousState);
-                            if(!removed.contains(nextPair) && !supportQueue.contains(nextPair)) {
-                                transitionQueue.add(nextQueueElement);
-                                supportQueue.add(nextPair);
-                            }
+                        } else {
+                            followSymbolString.append("$}");
                         }
-                    } else {
-                        if(currentQueueElement.getRight() != -1) {
-                            Pair<Integer,String> transitionPair = new Pair<>(currentQueueElement.getRight(), "$");
-                            addTransition(stateCounter, transitions, transitionPair);
+                        Pair<String,String> nextPair = new Pair<>(transitionSymbol, followSymbolString.toString());
+                        Pair<Pair<String,String>,Integer> nextQueueElement = new Pair<>(nextPair,previousState);
+                        if(!removed.contains(nextPair) && !supportQueue.contains(nextPair)) {
+                            transitionQueue.add(nextQueueElement);
+                            supportQueue.add(nextPair);
                         }
                     }
-                    if(currentProductions.indexOf("*") != currentProductions.size() -1) {
-                        previousState = stateCounter - 1;
-                        transitionSymbol = currentProductions.get(currentProductions.indexOf("*") + 1);
+                } else {
+                    if(currentQueueElement.getRight() != -1) {
+                        Pair<Integer,String> transitionPair = new Pair<>(currentQueueElement.getRight(), "$");
+                        addTransition(stateCounter, transitions, transitionPair);
                     }
                 }
+                if(currentProductions.indexOf("*") != currentProductions.size() -1) {
+                    previousState = stateCounter - 1;
+                    transitionSymbol = currentProductions.get(currentProductions.indexOf("*") + 1);
+                }
             }
-            //printEnka(productionsToStates,transitions);
         }
+        //printEnka(productionsToStates,transitions);
+        return new Pair<>(productionsToStates,transitions);
+    }
 
     private void printEnka(Map<Integer, Pair<Pair<String, List<String>>, String>> productionsToStates, Map<Pair<Integer, String>, List<Integer>> transitions) {
         for(Map.Entry<Pair<Integer,String>,List<Integer>> entry : transitions.entrySet()) {
             Pair<Pair<String,List<String>>,String> currentState = productionsToStates.get(entry.getKey().getLeft());
-            StringBuilder currentStateString = new StringBuilder(currentState.getLeft().getLeft() + " -> ");
+            StringBuilder currentStateString = new StringBuilder(entry.getKey().getLeft() + ". ");
+            currentStateString.append(currentState.getLeft().getLeft() + " -> ");
             for(String s : currentState.getLeft().getRight()) {
                 currentStateString.append(s);
             }
@@ -281,7 +350,7 @@ public class ParserGenerator {
             currentStateString.append(currentState.getRight());
             for(Integer nextState: entry.getValue()) {
                 Pair<Pair<String,List<String>>,String> rightSideState = productionsToStates.get(nextState);
-                StringBuilder currentRightSideStateString = new StringBuilder(rightSideState.getLeft().getLeft() + " -> ");
+                StringBuilder currentRightSideStateString = new StringBuilder(nextState + ". " + rightSideState.getLeft().getLeft() + " -> ");
                 for(String s : rightSideState.getLeft().getRight()) {
                     currentRightSideStateString.append(s);
                 }
