9b5983a6a050c2ae631def1555578ffe6b0f5ec8
Matej Čubek <33391902+MCubek@users.noreply.github.com>
2020-12-27 10:48:39 +0100

    Added SemanticProduction.java

---
+++ Lab/src/main/java/ppj/lab3/utilities/SemanticProduction.java
@@ -0,0 +1,73 @@
+package ppj.lab3.utilities;
+
+import ppj.lab3.utilities.symbols.NonTerminalSymbol;
+import ppj.lab3.utilities.symbols.Symbol;
+import ppj.utilities.Node;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author MatejCubek, FraneB
+ * @project PPJLab
+ * @created 27/12/2020
+ */
+public class SemanticProduction {
+    private final Node<Symbol> leftState;
+    private final List<Node<Symbol>> rightStates;
+
+    public SemanticProduction(Node<Symbol> leftStateNode) {
+        if (! (leftStateNode.getValue() instanceof NonTerminalSymbol))
+            throw new IllegalArgumentException("Left state must be nonTerminal.");
+
+        this.leftState = leftStateNode;
+        this.rightStates = generateRightStates(leftStateNode);
+    }
+
+    public String getLeftStateValue() {
+        return leftState.getValue().getSymbolName();
+    }
+
+    public List<String> getRightStateValues() {
+        return rightStates.stream()
+                .map(v -> v.getValue().getSymbolName())
+                .collect(Collectors.toList());
+    }
+
+    public List<Symbol> getRightStates() {
+        return rightStates.stream()
+                .map(Node::getValue)
+                .collect(Collectors.toList());
+    }
+
+    public Node<Symbol> getLeftStateNode() {
+        return leftState;
+    }
+
+    public List<Node<Symbol>> getRightStateNodes() {
+        return rightStates;
+    }
+
+    public static List<Node<Symbol>> generateRightStates(Node<Symbol> leftState) {
+        return List.copyOf(leftState.getChildren());
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SemanticProduction that = (SemanticProduction) o;
+
+        if (! getLeftStateValue().equals(that.getLeftStateValue())) return false;
+        return getRightStateValues().equals(that.getRightStateValues());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getLeftStateValue().hashCode();
+        result = 31 * result + getRightStateValues().hashCode();
+        return result;
+    }
+}
