f04203291048bf86382378e257402c7bbe4a24ba
Matej Čubek <33391902+MCubek@users.noreply.github.com>
2021-01-08 23:34:58 +0100

    Added Tests

---
+++ Lab/src/main/java/ppj/lab4/GeneratorKoda.java
@@ -0,0 +1,24 @@
+package ppj.lab4;
+
+import java.nio.file.Path;
+
+/**
+ * @author MatejCubek
+ * @project PPJLab
+ * @created 08/01/2021
+ */
+public class GeneratorKoda {
+
+    public GeneratorKoda(Path inputPath) {
+        // TODO: 8.1.2021.  
+    }
+
+    public String getAssembly() {
+        // TODO: 8.1.2021.
+        return "";
+    }
+
+    public static void main(String[] args) {
+        // TODO: 8.1.2021.
+    }
+}
