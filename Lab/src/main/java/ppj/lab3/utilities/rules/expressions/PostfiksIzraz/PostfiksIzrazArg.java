0fe9ed324d7356925187523cc1226110ec53214b
Matej Čubek <33391902+MCubek@users.noreply.github.com>
2021-01-23 01:27:14 +0100

    including 14

--- Lab/src/main/java/ppj/lab3/utilities/rules/expressions/PostfiksIzraz/PostfiksIzrazArg.java
+++ Lab/src/main/java/ppj/lab3/utilities/rules/expressions/PostfiksIzraz/PostfiksIzrazArg.java
@@ -8,6 +8,8 @@ import ppj.lab3.utilities.rules.Action;
 import ppj.lab3.utilities.rules.RuleFactory;
 import ppj.lab3.utilities.scope.Scope;
 import ppj.lab3.utilities.symbols.NonTerminalSymbol;
+import ppj.lab3.utilities.symbols.TerminalSymbol;
+import ppj.lab4.GeneratorKoda;
 
 public class PostfiksIzrazArg implements Action {
 
@@ -17,26 +19,26 @@ public class PostfiksIzrazArg implements Action {
         //1. provjeri(<postfiks_izraz>)
         SemanticProduction productionToCheck = new SemanticProduction(production.getRightStateNodes().get(0));
         //pozovi funkciju iz mape
-        RuleFactory ruleFactory= RuleFactory.getRuleFactory();
-        Action action= ruleFactory.getRuleMap().get(productionToCheck);
-        action.checkProduction(productionToCheck,scope);
+        RuleFactory ruleFactory = RuleFactory.getRuleFactory();
+        Action action = ruleFactory.getRuleMap().get(productionToCheck);
+        action.checkProduction(productionToCheck, scope);
         NonTerminalSymbol expression = (NonTerminalSymbol) production.getRightStates().get(0);
         String typePostfix = expression.getAttributeMap().get("type").getAttribute().toString();
 
         //2. provjeri(<lista_argumenata>)
         productionToCheck = new SemanticProduction(production.getRightStateNodes().get(2));
-        action= ruleFactory.getRuleMap().get(productionToCheck);
-        action.checkProduction(productionToCheck,scope);
+        action = ruleFactory.getRuleMap().get(productionToCheck);
+        action.checkProduction(productionToCheck, scope);
         expression = (NonTerminalSymbol) production.getRightStates().get(2);
 
         //3. <postfiks_izraz>.tip = funkcija(params -> pov) i redom po elementima
         //arg-tip iz <lista_argumenata>.tipovi i param-tip iz params vrijedi arg-tip
         //?= param-tip
-        if(!typePostfix.contains("funkcija(")) {
+        if (! typePostfix.contains("funkcija(")) {
             throw new SemanticException(production.toString());
         }
 
-        typePostfix = typePostfix.replace("funkcija(","");
+        typePostfix = typePostfix.replace("funkcija(", "");
         String trimmedTypePostfix = typePostfix.substring(0, typePostfix.indexOf("-")).trim();
         String[] params = trimmedTypePostfix.split(",");
         ListAttribute argTypesAttribute = (ListAttribute) expression.getAttributeMap().get("types");
@@ -58,5 +60,18 @@ public class PostfiksIzrazArg implements Action {
         production.getLeftState().addAttribute("type", new SimpleAttribute(pov));
         production.getLeftState().addAttribute("lExpression", new SimpleAttribute("false"));
 
+        GeneratorKoda.codeBuilder.addCommand("PUSH R6");
+
+        String functionName = ((TerminalSymbol) production.getRightStateNodes().get(0).getChildren().get(0).getChildren().get(0).getValue()).getLexicalUnits()[0];
+
+        GeneratorKoda.codeBuilder.addCommand("CALL " + GeneratorKoda.getFunctionLabel(functionName));
+
+        GeneratorKoda.codeBuilder.addCommand("MOVE R6, R0");
+        GeneratorKoda.codeBuilder.addCommand("POP R6");
+
+
+        GeneratorKoda.codeBuilder.addCommand("ADD R7, %D " + 4 * params.length + ", R7");
+
+        GeneratorKoda.codeBuilder.addCommand("PUSH R0");
     }
 }
--- Lab/src/main/java/ppj/lab3/utilities/rules/expressions/PrimarniIzraz/PrimarniIzrazIdn.java
+++ Lab/src/main/java/ppj/lab3/utilities/rules/expressions/PrimarniIzraz/PrimarniIzrazIdn.java
@@ -6,8 +6,12 @@ import ppj.lab3.utilities.attributes.SimpleAttribute;
 import ppj.lab3.utilities.rules.Action;
 import ppj.lab3.utilities.scope.Scope;
 import ppj.lab3.utilities.scope.ScopeElement;
+import ppj.lab3.utilities.symbols.Symbol;
 import ppj.lab3.utilities.symbols.TerminalSymbol;
 import ppj.lab4.GeneratorKoda;
+import ppj.utilities.Node;
+
+import java.util.LinkedList;
 
 public class PrimarniIzrazIdn implements Action {
 
@@ -27,10 +31,38 @@ public class PrimarniIzrazIdn implements Action {
         }
 
         if (! foundElement.getType().contains("funkcija")) {
-            GeneratorKoda.codeBuilder.addCommand("MOVE " + GeneratorKoda.getGlobalLabel(name) + ", R0");
 
+            if (scope.getElements().size() == 0) {
+                GeneratorKoda.codeBuilder.addCommand("MOVE " + GeneratorKoda.getGlobalLabel(name) + ", R0");
+            } else {
+                //TODO Što se dogada?
+                GeneratorKoda.codeBuilder.addCommand("ADD R6, %D " + 4 * getNumberOfElements(production.getLeftStateNode()) + ", R0");
+            }
             GeneratorKoda.codeBuilder.addCommand("PUSH R0");
         }
 
+    }
+
+    private static int getNumberOfElements(Node<Symbol> startingNode) {
+        Node<Symbol> curr = startingNode;
+        while (curr != null && ! curr.getValue().getSymbolName().equals("<jednakosni_izraz>")) {
+            curr = curr.getParent();
+        }
+
+        int count = 0;
+
+        LinkedList<Node<Symbol>> stack = new LinkedList<>();
+        stack.add(curr);
+
+        while (! stack.isEmpty()) {
+            var el = stack.pop();
+
+            if (el.getValue().getSymbolName().equals("<primarni_izraz>"))
+                count++;
+
+            if (el.getChildren() != null)
+                stack.addAll(el.getChildren());
+        }
+        return count;
     }
 }
