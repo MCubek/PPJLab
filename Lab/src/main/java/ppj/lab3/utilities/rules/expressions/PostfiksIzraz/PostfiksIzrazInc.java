156c90ba61a9a3063493db673b75606f58e4b927
Matej Čubek <33391902+MCubek@users.noreply.github.com>
2021-01-24 15:10:57 +0100

    31 added

--- Lab/src/main/java/ppj/lab3/utilities/rules/expressions/PostfiksIzraz/PostfiksIzrazInc.java
+++ Lab/src/main/java/ppj/lab3/utilities/rules/expressions/PostfiksIzraz/PostfiksIzrazInc.java
@@ -7,6 +7,7 @@ import ppj.lab3.utilities.rules.Action;
 import ppj.lab3.utilities.rules.RuleFactory;
 import ppj.lab3.utilities.scope.Scope;
 import ppj.lab3.utilities.symbols.NonTerminalSymbol;
+import ppj.lab4.GeneratorKoda;
 
 public class PostfiksIzrazInc implements Action {
 
@@ -16,9 +17,9 @@ public class PostfiksIzrazInc implements Action {
         //1. provjeri(<postfiks_izraz>)
         SemanticProduction productionToCheck = new SemanticProduction(production.getRightStateNodes().get(0));
         //pozovi funkciju iz mape
-        RuleFactory ruleFactory= RuleFactory.getRuleFactory();
-        Action action= ruleFactory.getRuleMap().get(productionToCheck);
-        action.checkProduction(productionToCheck,scope);
+        RuleFactory ruleFactory = RuleFactory.getRuleFactory();
+        Action action = ruleFactory.getRuleMap().get(productionToCheck);
+        action.checkProduction(productionToCheck, scope);
         NonTerminalSymbol expression = (NonTerminalSymbol) production.getRightStates().get(0);
 
         //2. <postfiks_izraz>.l-izraz = 1 i <postfiks_izraz>.tip ?= int
@@ -33,5 +34,19 @@ public class PostfiksIzrazInc implements Action {
         //l-izraz <- 0
         production.getLeftState().addAttribute("type", new SimpleAttribute("int"));
         production.getLeftState().addAttribute("lExpression", new SimpleAttribute("false"));
+
+        GeneratorKoda.codeBuilder.addCommand("POP R1");
+        GeneratorKoda.codeBuilder.addCommand("LOAD R0, (R1)");
+        GeneratorKoda.codeBuilder.addCommand("PUSH R0");
+
+        if ("OP_INC".equals(production.getRightStates().get(0).getSymbolName())) {
+            GeneratorKoda.codeBuilder.addCommand("ADD R0, 1, R0");
+        } else {
+            GeneratorKoda.codeBuilder.addCommand("SUB R0, 1, R0");
+        }
+
+        GeneratorKoda.codeBuilder.addCommand("STORE R0, (R1)");
     }
+
+
 }
